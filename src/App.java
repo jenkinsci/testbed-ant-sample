36ce130e7da56fe1ab2c7620ce4f245bc716f3cd
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-11-26 13:40:27 -0800

    added findbugs

--- src/App.java
+++ src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public boolean thisCausesFindBugWarnings(String s) {
+        return "abc"==s;
+    }
 }
